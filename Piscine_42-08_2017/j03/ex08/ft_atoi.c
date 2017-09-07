934437c544227a39fe15c883b70394fcc15aed21
bvalcasa <31510683+bvalcasa@users.noreply.github.com>
2017-09-07 13:23:47 +0200

    Piscine 42

--- Piscine_42-08_2017/j03/ex08/ft_atoi.c
+++ Piscine_42-08_2017/j03/ex08/ft_atoi.c
@@ -6,10 +6,10 @@
 /*   By: bvalcasa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/07 01:17:53 by bvalcasa          #+#    #+#             */
-/*   Updated: 2017/09/07 06:48:26 by bvalcasa         ###   ########.fr       */
+/*   Updated: 2017/09/07 08:02:04 by bvalcasa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
+
 int		ft_atoi(char *str)
 {
 	int s;
@@ -33,20 +33,5 @@ int		ft_atoi(char *str)
 		result += str[s] - '0';
 		s++;
 	}
-	if (negative == 1)
-	{
-		return (-result);
-	}
-	else
-	{
-		return (result);
-	}
-}
-
-int		main(void)
-{
-	char c[] = "-40";
-	ft_atoi(c);
-	printf("%d",ft_atoi(c));
-	return (0);
+	return (negative == 1 ? -result : result);
 }
